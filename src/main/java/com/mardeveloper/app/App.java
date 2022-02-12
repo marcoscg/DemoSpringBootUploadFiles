15a412529962a0e2028887a948b96146f52dae08
Marcos <guimaraes.mar@gmail.com>
2022-02-12 19:00:07 -0300

    Project Upload Files

---
+++ src/main/java/com/mardeveloper/app/App.java
@@ -0,0 +1,27 @@
+package com.mardeveloper.app;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mardeveloper.app.service.FilesStorageService;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+	
+	@Resource
+	FilesStorageService storageService;	
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	    storageService.deleteAll();
+	    storageService.init();
+	}
+
+}
